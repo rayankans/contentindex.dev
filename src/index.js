825918f05eafa2cd1051ae181c2509b65c5dd922
Rayan Kanso <rayankans@chromium.org>
2019-07-14 12:02:22 +0100

    Remove default react files, create app bar + 2 tabs

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import TopAppBar from './TopAppBar';
+import FullWidthTabs from './FullWidthTabs';
+
+function App() {
+  return (
+  <>
+    <TopAppBar />
+    <FullWidthTabs />
+  </>);
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
