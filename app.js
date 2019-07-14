df7e40c0a706dc3397875a882afd39d43a38dd21
Rayan Kanso <rayankans@chromium.org>
2019-07-14 15:09:57 +0100

    Use concurrently to simplify development

--- app.js
+++ app.js
@@ -7,7 +7,10 @@ const port = 5000;
 
 // API endpoint.
 app.all('/api', (req, res) => res.json('["asDasdasDAsdas"]'));
-app.get('/static/*', (req, res) => res.sendFile(path.join(__dirname, `client/build/${req.path}`)));
-app.get('/*', (req, res) => res.sendFile(path.join(__dirname, 'client/build/index.html')));
+
+if (app.get('env') === 'production') {
+  app.get('/static/*', (req, res) => res.sendFile(path.join(__dirname, `client/build/${req.path}`)));
+  app.get('/*', (req, res) => res.sendFile(path.join(__dirname, 'client/build/index.html')));
+}
 
 app.listen(port, () => console.log(`Listening on port ${port}! Mode: ${app.get('env')}`));
