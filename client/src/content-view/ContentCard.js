81125ecb3c4a5e893433fd6b3bc9b6f5fdd8e32c
Rayan Kanso <rayankans@chromium.org>
2019-07-22 22:37:01 +0100

    More CSS touchups

--- client/src/content-view/ContentCard.js
+++ client/src/content-view/ContentCard.js
@@ -11,41 +11,9 @@ import CheckIcon from '@material-ui/icons/Check';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 import { saveArticle, deleteArticle } from '../redux/actions';
-
-const imageDimension = '15vh';
-const useStyles = makeStyles(theme => ({
-  card: {
-    position: 'relative',
-    width: '100%',
-    height: imageDimension,
-  },
-  actionarea: {
-    display: 'flex',
-    justifyContent: 'left',
-    width: '100%',
-    height: '100%',
-  },
-  media: {
-    flex: `0 0 ${imageDimension}`,
-    height: imageDimension,
-  },
-  metadata: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'flex-start',
-    flex: '1 1 auto',
-    height: imageDimension,
-    padding: theme.spacing(1),
-  },
-  actions: {
-    position: 'absolute',
-    padding: theme.spacing(0),
-    bottom: theme.spacing(1),
-    right: theme.spacing(1),
-  },
-}));
 
 /** @enum {string} */
 const SaveState = {
@@ -95,8 +63,41 @@ async function deleteContent(article, setSaveState, dispatch) {
 }
 
 function ContentCard(props) {
-  const classes = useStyles();
   const theme = useTheme();
+  const imageDimension = useMediaQuery(theme.breakpoints.up('md')) ? '10vw' : '25vw';
+
+  const classes = makeStyles(theme => ({
+    card: {
+      position: 'relative',
+      width: '100%',
+      height: imageDimension,
+    },
+    actionarea: {
+      display: 'flex',
+      justifyContent: 'left',
+      width: '100%',
+      height: '100%',
+    },
+    media: {
+      flex: `0 0 ${imageDimension}`,
+      height: imageDimension,
+      padding: theme.spacing(2),
+    },
+    metadata: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'flex-start',
+      flex: '1 1 auto',
+      height: imageDimension,
+      padding: theme.spacing(2),
+    },
+    actions: {
+      position: 'absolute',
+      padding: theme.spacing(0),
+      bottom: theme.spacing(1),
+      right: theme.spacing(1),
+    },
+  }))();
 
   const isSaved = props.savedArticles.map(a => a.url).includes(props.article.url);
   const [saveState, setSaveState] = React.useState(isSaved ? SaveState.SAVED : SaveState.CAN_SAVE);
@@ -137,7 +138,7 @@ function ContentCard(props) {
       </CardActionArea>
       <CardActions className={classes.actions} disableSpacing>
         <Button 
-            size="medium" color="primary"
+            color="primary"
             style={{ backgroundColor: 'transparent', padding: 0, minWidth: 0 }}
             onClick={() => handleClick(props.article, saveState, setSaveState, props.dispatch)}
         >
--- client/src/content-view/ContentCardGrid.js
+++ client/src/content-view/ContentCardGrid.js
@@ -15,16 +15,15 @@ const useStyles = makeStyles(theme => ({
 export default function ContentCards(props) {
   const classes = useStyles();
   const theme = useTheme();
-  const matches = useMediaQuery(theme.breakpoints.up('md'));
-  const spacing = matches ? '200px' : theme.spacing(2);
+  const spacing = useMediaQuery(theme.breakpoints.up('md')) ? '200px' : theme.spacing(2);
 
   return (
   <Grid container className={classes.root} style={{ paddingLeft: spacing, paddingRight: spacing }} >
     <Grid item xs={12}>
       <Grid container justify="center" spacing={4}>
         {props.articles.map((article, i) => (
-          <Grid key={i} item xs={12} lg={6} spacing={2}>
-            <ContentCard article={article} isMd={matches}/>
+          <Grid key={i} item xs={12} lg={6}>
+            <ContentCard article={article} />
           </Grid>
         ))}
       </Grid>
