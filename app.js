db8be37972f6f60bff71c3a25ba0411c50f67326
Rayan Kanso <rayankans@chromium.org>
2019-07-23 01:16:35 +0100

    Fix express static serving

--- app.js
+++ app.js
@@ -10,9 +10,8 @@ const api = require('./api/api.js');
 app.use('/api', api);
 
 if (app.get('env') === 'production') {
-  app.get('/static/*', (req, res) => res.sendFile(path.join(__dirname, `client/build/${req.path}`)));
-  app.get('/$', (req, res) => res.sendFile(path.join(__dirname, 'client/build/index.html')));
-  app.get('/*', (req, res) => res.sendFile(path.join(__dirname, `client/build/${req.path}`)));
+  app.use(express.static('client/build'));
+  app.get('/*', (req, res) => res.sendFile(path.join(__dirname, 'client/build/index.html')));
 }
 
 app.listen(port, () => console.log(`Listening on port ${port}! Mode: ${app.get('env')}`));
