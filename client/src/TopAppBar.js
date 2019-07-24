09fec95214385b5d5a393ae6c850d7d616b1008b
Rayan Kanso <rayankans@chromium.org>
2019-07-24 01:48:55 +0100

    Display offline media

--- client/src/TopAppBar.js
+++ client/src/TopAppBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom'
-import { makeStyles, responsiveFontSizes } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import IconButton from '@material-ui/core/IconButton';
 import SvgIcon from '@material-ui/core/SvgIcon';
--- client/src/view/OfflineArticle.js
+++ client/src/view/OfflineArticle.js
@@ -1,18 +1,50 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    textAlign: 'center',
+    [theme.breakpoints.up('md')]: {
+      paddingLeft: '10vw',
+      paddingRight: '10vw',
+    },
+    [theme.breakpoints.down('sm')]: {
+      paddingLeft: '5vw',
+      paddingRight: '5vw',
+    },
+  },
+  media: {
+    maxWidth: '100%',
+    maxHeight: '70vh',
+  },
+}));
+
+function PhotoMedia(props) {
+  const classes = useStyles();
+  return <img alt="" src={`${props.url}?cache`} className={classes.media}/>;
+}
+
+function VideoMedia(props) {
+  const classes = useStyles();
+
+  return (
+  <video style={{maxWidth: "100%"}} controls className={classes.media}>
+    <source src={`${props.url}?cache`} type="video/mp4" />
+  </video>);
+}
 
 export default function OfflineArticle(props) {
+  const classes = useStyles();
+
   const id = props.match.params[0];
   const article = JSON.parse(localStorage.getItem(id));
   if (!article)
     return (<h1> 404 </h1>);
 
-  if (article.type === 'photo') {
-    return (<div style={{textAlign: 'center'}}>
+  return (
+    <div className={classes.root}>
       <h1> {article.title} </h1>
-      <h2> {article.description} </h2>
-      <img alt="" src={`${article.url}?cache`} />
+      <h3> {article.description} </h3>
+      {article.type === 'video' ? <VideoMedia url={article.url} /> : <PhotoMedia url={article.url} />}
     </div>);
-  }
-
-  return (<h1> {JSON.stringify(article, null, 2)} </h1>);
 }
