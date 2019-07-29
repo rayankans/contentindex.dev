6725369f2217855365a8630d0c9bf85c05fa85ef
Rayan Kanso <rayankans@chromium.org>
2019-07-29 23:43:36 +0100

    Improve offline page media UI

--- client/src/view/OfflineArticle.js
+++ client/src/view/OfflineArticle.js
@@ -1,22 +1,35 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
 const useStyles = makeStyles(theme => ({
   root: {
     textAlign: 'center',
     paddingTop: theme.spacing(4),
     [theme.breakpoints.up('md')]: {
+      paddingLeft: '20vw',
+      paddingRight: '20vw',
+    },
+    [theme.breakpoints.down('sm')]: {
       paddingLeft: '10vw',
       paddingRight: '10vw',
     },
-    [theme.breakpoints.down('sm')]: {
-      paddingLeft: '5vw',
-      paddingRight: '5vw',
-    },
+  },
+  title: {
+    paddingTop: theme.spacing(1),
+    paddingBottom: theme.spacing(2),
   },
   media: {
-    maxWidth: '100%',
+    width: '100%',
     maxHeight: '70vh',
+    borderRadius: '5px',
   },
 }));
 
@@ -27,11 +40,42 @@ function PhotoMedia(props) {
 
 function VideoMedia(props) {
   const classes = useStyles();
+  return (
+    <video controls className={classes.media}>
+      <source src={`/content/${props.id}`} type="video/mp4" />
+    </video>);
+}
+
+function MediaCard(props) {
+  const classes = useStyles();
 
   return (
-  <video controls className={classes.media}>
-    <source src={`/content/${props.id}`} type="video/mp4" />
-  </video>);
+    <Card className={classes.card}>
+      <CardContent className={classes.title}>
+        <Typography variant="h5" component="h2">
+          {props.article.title}
+        </Typography>
+        <Typography variant="body2" color="textSecondary" component="p">
+          {props.article.description}
+        </Typography>
+      </CardContent>
+      <CardActionArea>
+        <CardMedia>
+          {props.article.type === 'photo'
+              ? <PhotoMedia id={props.article.id} />
+              : <VideoMedia id={props.article.id} />}
+        </CardMedia>
+      </CardActionArea>
+      <CardActions>
+        <Button 
+            size="small" color="primary" disabled={!Boolean(props.article.permalink)}
+            onClick={() => window.open(props.article.permalink, '_blank')}
+        >
+          Source
+        </Button>
+      </CardActions>
+    </Card>
+  );
 }
 
 export default function OfflineArticle(props) {
@@ -47,8 +91,6 @@ export default function OfflineArticle(props) {
 
   return (
     <div className={classes.root}>
-      <h1> {article.title} </h1>
-      <h3> {article.description} </h3>
-      {article.type === 'video' ? <VideoMedia id={article.id} /> : <PhotoMedia id={article.id} />}
+      <MediaCard article={article} />
     </div>);
 }
